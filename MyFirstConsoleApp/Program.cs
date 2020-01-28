7b94ecbf604214db6f1971ef1ac9ffaa39a39ad0
Veronica <veronicavitu15@gmail.com>
2020-01-28 20:36:43 +0200

    Am modificat comentariul in proiect

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); //prints a text to the console
+            Console.Read(); //holds the console open ntil a key is a pressed
+        }
+    }
+}
